5e4d142cefc2fa4baa2e2597ae3e7c3c463c79c9
Gustavo Dias <dias.gustavo23@gmail.com>
2020-06-25 10:10:45 -0300

    Finalizado

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Aula25Recursividade
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recursividade sequencia = new Recursividade();
+            System.Console.WriteLine($"Sequência Fibonacci:");
+            sequencia.GerarSequenciaFibonacci(0, 1, 10);
+            System.Console.WriteLine();
+            Console.WriteLine($"Fatorial: {sequencia.GerarFatorial(8)}");
+        }
+    }
+}
